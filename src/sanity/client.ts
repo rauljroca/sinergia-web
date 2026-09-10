eb1d54b4d97e36544ae708e5bf0f1d06246aef1a
rroca <rroca@bitban.com>
2026-09-11 01:32:52 +0200

    mejoras filtros

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from 'next-sanity'
 
 export const client = createClient({
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-    apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'q6wo9gxa',
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+    apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2026-01-01',
     useCdn: false, // En false para que veas los cambios al instante
 })
